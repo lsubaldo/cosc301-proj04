a42bb876261a1ba65cbe2595a4dbbd482d366d60
Cary Baun <cbaun@colgate.edu>
2015-11-10 13:00:34 -0500

    Made changes to uthread.c for release/acquire of locks

--- uthread.c
+++ uthread.c
@@ -8,17 +8,28 @@
 /*
  * This is where you'll need to implement the user-level functions
  */
-int ustack; 
+int ustack;  //only for single thread 
 	
 void lock_init(lock_t *lock) {
 	lock->flag = 0;
 }
 
 void lock_acquire(lock_t *lock) {
-	
+	if(holding(lock)) {
+		panic("acquire");
+	} 
+
+	while(xchg(&lock->flag,1) != 0){		//spin wait 
+		;
+	}
 }
 
 void lock_release(lock_t *lock) {
+	if(!holding(lock)) {
+		panic("release");
+	}
+
+	xchg(&lock->flag,0);
 }
 
 int thread_join(int pid) {
@@ -28,7 +39,7 @@ int thread_join(int pid) {
 }
 
 int thread_create(void (*start_routine)(void *), void *arg) {
-	ustack = (int)malloc(sizeof(int) * (PGSIZE));
+	ustack = (int)malloc(PGSIZE*2);		
     int pid = clone(*start_routine, arg, (void*)ustack);
 	return pid; 
 }
